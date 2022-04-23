69014231ef2bd0d9310749391a729b5d05ed72a9
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-23 22:36:55 +0800

    feat: add `register` nav button

--- app/src/main/java/com/codewithjosh/Swift2k22/LoginActivity.java
+++ app/src/main/java/com/codewithjosh/Swift2k22/LoginActivity.java
@@ -1,14 +1,34 @@
 package com.codewithjosh.Swift2k22;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText _username, _password;
+    Button _onLogin;
+    TextView _onSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        _username = findViewById(R.id.username);
+        _password = findViewById(R.id.password);
+        _onLogin = findViewById(R.id.on_login);
+        _onSignUp = findViewById(R.id.on_sign_up);
+
+        _onSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            finish();
+        });
+
     }
+
 }
