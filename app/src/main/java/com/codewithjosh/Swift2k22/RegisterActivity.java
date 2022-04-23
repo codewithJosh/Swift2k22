8612081ba8238bcce830a4829fb0bce137bdf419
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-23 22:46:18 +0800

    feat: add `login` nav

--- app/src/main/java/com/codewithjosh/Swift2k22/RegisterActivity.java
+++ app/src/main/java/com/codewithjosh/Swift2k22/RegisterActivity.java
@@ -1,14 +1,36 @@
 package com.codewithjosh.Swift2k22;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText _username, _email, _password, _rePassword;
+    Button _onRegister;
+    TextView _onSignIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        _username = findViewById(R.id.username);
+        _email = findViewById(R.id.email);
+        _password = findViewById(R.id.password);
+        _rePassword = findViewById(R.id.rePassword);
+        _onRegister = findViewById(R.id.on_register);
+        _onSignIn = findViewById(R.id.on_sign_in);
+
+        _onSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
+        });
+
     }
+
 }
