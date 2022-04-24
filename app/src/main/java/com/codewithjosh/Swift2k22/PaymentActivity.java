91399fe9da74a55a5de51dbef432b7bb67fb0e9f
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-24 15:39:21 +0800

    feat: load `bundle`
    Include the following:
    - load `s_route_name` into `tv_route_name`
    - load `i_bus_number` into `tv_bus_number`
    - load `i_bus_fare` into `tv_bus_fare`, `tv_total_amount` and `payment` button

--- app/src/main/java/com/codewithjosh/Swift2k22/PaymentActivity.java
+++ app/src/main/java/com/codewithjosh/Swift2k22/PaymentActivity.java
@@ -1,14 +1,46 @@
 package com.codewithjosh.Swift2k22;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PaymentActivity extends AppCompatActivity {
 
+    Button _onPay;
+    TextView _routeName, _busNumber, _userBalance, _busFare, _totalAmt;
+
+    String route_name, bus_number, bus_fare, bus_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        _onPay = findViewById(R.id.on_pay);
+        _routeName = findViewById(R.id.route_name);
+        _busNumber = findViewById(R.id.bus_number);
+        _userBalance = findViewById(R.id.user_balance);
+        _busFare = findViewById(R.id.bus_fare);
+        _totalAmt = findViewById(R.id.total_amt);
+
+        bus_id = getIntent().getStringExtra("bus_id");
+        route_name = getIntent().getStringExtra("route_name");
+        bus_number = getIntent().getStringExtra("bus_number");
+        bus_fare = getIntent().getStringExtra("bus_fare");
+
+        if (route_name != null) {
+
+            final String _fare = "PHP "+ bus_fare +".00";
+
+            _routeName.setText(route_name);
+            _busNumber.setText("BUS NO. " + bus_number);
+            _busFare.setText(_fare);
+            _totalAmt.setText(_fare);
+            _onPay.setText("PAY " + _fare);
+        }
+
     }
+
 }
