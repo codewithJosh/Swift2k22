09c96c06489e509263f4589badda3b39c19826d3
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-24 13:37:33 +0800

    feat: load `bundle`
    Include the following:
    - load `s_route_name` into `tv_route_name`
    - load `s_route_timestamp` into `tv_route_timestamp`

--- app/src/main/java/com/codewithjosh/Swift2k22/ReservationActivity.java
+++ app/src/main/java/com/codewithjosh/Swift2k22/ReservationActivity.java
@@ -1,14 +1,33 @@
 package com.codewithjosh.Swift2k22;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ReservationActivity extends AppCompatActivity {
+
+    RecyclerView _recyclerBus;
+    TextView _routeName, _routeTimestamp;
+
+    String route_id, route_name, route_timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+
+        _recyclerBus = findViewById(R.id.recycler_bus);
+        _routeName = findViewById(R.id.route_name);
+        _routeTimestamp = findViewById(R.id.bus_date_timestamp);
+
+        route_id = getIntent().getStringExtra("route_id");
+        route_name = getIntent().getStringExtra("route_name");
+
+        _routeName.setText(route_name);
+        _routeTimestamp.setText(route_timestamp);
+
     }
+
 }
