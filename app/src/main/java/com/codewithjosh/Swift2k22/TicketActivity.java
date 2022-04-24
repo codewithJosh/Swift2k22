37ff19c02c1925d4df230b84cdfa14220810d1b3
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-24 19:02:15 +0800

    feat: load `bus_timestamp` into `tv_bus_timestamp`

--- app/src/main/java/com/codewithjosh/Swift2k22/TicketActivity.java
+++ app/src/main/java/com/codewithjosh/Swift2k22/TicketActivity.java
@@ -7,12 +7,22 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class TicketActivity extends AppCompatActivity {
 
     ImageView _barcode;
     TextView _busFare, _totalAmt, _ref, _busTimestamp;
 
     String bus_fare, bus_id, ticket_id;
+
+    FirebaseFirestore firebaseFirestore;
+
+    DateFormat formatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,22 @@ public class TicketActivity extends AppCompatActivity {
         _busFare.setText("PHP " + bus_fare + ".00");
         _totalAmt.setText("PHP " + bus_fare + ".00");
         _ref.setText("Ref No: " + ticket_id.toUpperCase());
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        firebaseFirestore
+                .collection("Buses")
+                .document(bus_id)
+                .get()
+                .addOnCompleteListener(task -> {
+
+                    if (task.isSuccessful()){
+
+                        DocumentSnapshot doc = task.getResult();
+                        formatter = new SimpleDateFormat("dd MMMM yyyy h:mm a");
+                        _busTimestamp.setText(formatter.format(doc.getDate("bus_timestamp")));
+                    }
+                });
 
     }
 
