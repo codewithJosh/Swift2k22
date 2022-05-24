2c7e48a97467ab9557dfef4e37ff6f81b98ffd6d
codewithJosh <dacasinjoshua21@gmail.com>
2022-05-24 21:29:45 +0800

    refactor: update `register` layout

--- app/src/main/java/com/codewithjosh/Swift2k22/RegisterActivity.java
+++ app/src/main/java/com/codewithjosh/Swift2k22/RegisterActivity.java
@@ -32,12 +32,12 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        _username = findViewById(R.id.username);
-        _email = findViewById(R.id.email);
-        _password = findViewById(R.id.password);
-        _rePassword = findViewById(R.id.rePassword);
-        _onRegister = findViewById(R.id.on_register);
-        _onSignIn = findViewById(R.id.on_sign_in);
+        _username = findViewById(R.id.et_user_name);
+        _email = findViewById(R.id.et_email);
+        _password = findViewById(R.id.et_password);
+        _rePassword = findViewById(R.id.et_re_password);
+        _onRegister = findViewById(R.id.btn_register);
+        _onSignIn = findViewById(R.id.nav_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
