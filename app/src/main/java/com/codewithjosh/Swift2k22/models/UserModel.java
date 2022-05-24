84d687ac7ed79e9c1befc9c619aa6cdd69339b0a
codewithJosh <dacasinjoshua21@gmail.com>
2022-05-24 21:29:15 +0800

    feat: add `UserModel` model

---
+++ app/src/main/java/com/codewithjosh/Swift2k22/models/UserModel.java
@@ -0,0 +1,54 @@
+package com.codewithjosh.Swift2k22.models;
+
+public class UserModel
+{
+
+    private int user_balance;
+    private String user_email;
+    private String user_id;
+    private String user_name;
+
+    public UserModel()
+    {
+
+    }
+
+    public UserModel(int user_balance, String user_email, String user_id, String user_name)
+    {
+
+        this.user_balance = user_balance;
+        this.user_email = user_email;
+        this.user_id = user_id;
+        this.user_name = user_name;
+
+    }
+
+    public int getUser_balance()
+    {
+
+        return user_balance;
+
+    }
+
+    public String getUser_email()
+    {
+
+        return user_email;
+
+    }
+
+    public String getUser_id()
+    {
+
+        return user_id;
+
+    }
+
+    public String getUser_name()
+    {
+
+        return user_name;
+
+    }
+
+}
